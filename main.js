449d404108ac6c7420189c000ab22fce145f4ae9
NUR AINA NAJWA BINTI MOKHTAR <101613483+ainanajwaa@users.noreply.github.com>
2022-05-31 04:15:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -25,21 +25,6 @@ app.post('/new', async (req, res) => {
 	console.log(req.body)
 	const hotel = await User.register(req.body.booking, req.body.type, req.body.name,req.body.pass);
 	res.json(hotel);
-	// if (search == null){
-	// 	console.log('Not Registered!')
-	// 	res.status(401).send({
-	// 		error: "Not Registered!"
-	// 	})
-	// 	}
-	// else{
-	// 	console.log("Booking is successfully registered")
-	// 	res.status(200).json({
-	// 		booking: search[0].booking,
-	// 		type: search[0].type,
-	// 		name: search[0].name
-
-	// 	})
-	// }
 })
 
 //Get/Read
@@ -47,19 +32,6 @@ app.get('/get', async (req, res) => {
 	console.log(req.body)
 	const hotel = await User.check(req.body.booking, req.body.type, req.body.name);
 	res.json(hotel);
-	// if (search == null){
-	// 	console.log('No registration found!')
-	// 	res.status(404).json()
-	// 	}	
-	// else{
-	// 	console.log("Registration found")
-	// 	res.status(200).json({
-	// 		booking: search[0].booking,
-	// 		type: search[0].type,
-	// 		name: search[0].name
-
-	// 	})
-	// }
 	});
 
 //Patch/Update
@@ -67,21 +39,6 @@ app.patch('/update', async (req, res) => {
 	console.log(req.body)
 	const hotel = await User.update(req.body.booking, req.body.type, req.body.name);
 	res.json(hotel)
-	// if (search == null){
-	// 	console.log('Booking not found')
-	// 	res.status(401).send({
-	// 		error: "No booking updated!"
-	// 	})
-	// 	}	
-	// else{
-	// 	console.log("Booking is successfully updated")
-	// 	res.status(200).json({
-	// 		booking: search[0].booking,
-	// 		type: search[0].type,
-	// 		name: search[0].name
-
-	// 	})
-	// }
 })
 
 //Delete/Delete
@@ -89,21 +46,6 @@ app.delete('/delete', async (req, res) => {
 	console.log(req.body)
 	const benr = await User.delete(req.body.booking, req.body.type, req.body.name);
 	res.json(benr)
-	// if (benr == null){
-	// 	console.log('Booking not found')
-	// 	res.status(404).send({
-	// 		error: "No booking found!"
-	// 	})
-	// 	}	
-	// else{
-	// 	console.log("Booking deleted")
-	// 	res.status(200).json({
-	// 		booking: search[0].booking,
-	// 		type: search[0].type,
-	// 		name: search[0].name
-
-	// 	})
-	// }
 })
 
 app.listen(port, () => {
